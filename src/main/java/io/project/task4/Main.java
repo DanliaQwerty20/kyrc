35a46acb971ae46aac8702a64956bd40acd65299
DKorchagin <DKorchagin@IBS.RU>
2024-08-26 10:23:09 +0300

    Исправление ошибок при трай-кетче

--- src/main/java/io/project/task4/Main.java
+++ src/main/java/io/project/task4/Main.java
@@ -1,29 +1,36 @@
 package io.project.task4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        Scanner scanner = new Scanner(System.in);
 
-        calculator.setNumber1(scanner.nextInt());
-        calculator.setNumber2(scanner.nextInt());
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите первое число: ");
+            calculator.setNumber1(scanner.nextDouble());
+            System.out.print("Введите второе число: ");
+            calculator.setNumber2(scanner.nextDouble());
 
-        calculator.setOperation(new Addition());
-        calculator.calculate();
-        System.out.println("Сложение: " + calculator.getResult());
+            calculator.setOperation(new Addition());
+            calculator.calculate();
+            System.out.println("Сложение: " + calculator.getResult());
 
-        calculator.setOperation(new Subtraction());
-        calculator.calculate();
-        System.out.println("Вычитание: " + calculator.getResult());
+            calculator.setOperation(new Subtraction());
+            calculator.calculate();
+            System.out.println("Вычитание: " + calculator.getResult());
 
-        calculator.setOperation(new Multiplication());
-        calculator.calculate();
-        System.out.println("Умножение: " + calculator.getResult());
+            calculator.setOperation(new Multiplication());
+            calculator.calculate();
+            System.out.println("Умножение: " + calculator.getResult());
 
-        calculator.setOperation(new Division());
-        calculator.calculate();
-        System.out.println("Деление: " + calculator.getResult());
+            calculator.setOperation(new Division());
+            calculator.calculate();
+            System.out.println("Деление: " + calculator.getResult());
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка ввода: пожалуйста, введите число.");
+        }
     }
 }
+
